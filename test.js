a2cb02dff164f775dad6e7911ca3a540229ecafd
Phongphun Kijsanayothin <kphongph@gmail.com>
2013-11-15 14:08:59 -0600

    add mongoclient

--- test.js
+++ test.js
@@ -5,9 +5,20 @@ var T = new Twit(config);
 
 var stream = T.stream('statuses/sample');
 var count = 0;
-stream.on('tweet', function(tweet) {
-  if(tweet.lang == 'th') {
-    console.log(count+' '+tweet.text);
-    count++;
-  }
+
+
+var MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect("mongodb://localhost:27017/admin", function(err, db) {
+  if(err) { return console.dir(err) };
+  db.collection('twitter', function(err, collection) {
+    var count = 0;
+    stream.on('tweet', function(tweet) {
+      collection.insert(tweet,{w:1}, function(err, result) {
+        console.log('tweet #'+count+' ');
+        count++;
+        if(err) { return console.dir(err) };
+      });
+    });
+  });
 });
