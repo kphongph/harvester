a05d1273409014d8a63f090e7c1049ffb37e0c33
unknown <Phongphun@CS306D.nu.local>
2013-11-15 14:32:41 -0600

    couchdb

--- test.js
+++ test.js
@@ -2,11 +2,22 @@ var Twit = require('twit');
 var config = require('./config1');
 
 var T = new Twit(config);
-
 var stream = T.stream('statuses/sample');
+
+var couchdb = require('felix-couchdb');
+client = couchdb.createClient(5984,'localhost');
+db = client.db('twitter');
+
 var count = 0;
+stream.on('tweet', function(tweet) {
+  console.log('tweet #'+count+' ');
+  count++;
+  db.saveDoc(tweet);
+});
 
 
+/*
+var count = 0;
 var MongoClient = require('mongodb').MongoClient;
 
 MongoClient.connect("mongodb://localhost:27017/admin", function(err, db) {
@@ -22,3 +33,6 @@ MongoClient.connect("mongodb://localhost:27017/admin", function(err, db) {
     });
   });
 });
+*/
+
+
